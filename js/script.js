0f99193e0a318b69ade310512f7959cc63dfbe4b
Igor-Mikhailov <51879551+Igor-Mikhailov@users.noreply.github.com>
2019-12-28 16:26:30 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,26 @@
 $(document).ready(function(){
-	var link = $('.navbar-burger');
-	var link_active = $('.navbar-burger_active');
-	var menu = $('.mobile-navigation');
-	var nav_link = $('.mobile-navigation a');
-
-	link.click(function(){
-		link.toggleClass('navbar-burger_active');
-		menu.toggleClass('mobile-navigation__active')
-	});
-	nav_link.click(function(){
-		link.toggleClass('navbar-burger_active');
-		menu.toggleClass('mobile-navigation__active');
-	});
+	$('.js-nav-menu-toggle').on('click', function() {
+    	$(this).parents('.navigation-menu').toggleClass('navigation-menu--open');
+  	});
+  
+  	$('html').on('click', function(e) {
+    	if(!$(e.target).closest('.js-nav-menu').length &&
+      		($('.js-nav-menu').hasClass('navigation-menu--open'))) {
+        	$('.js-nav-menu').removeClass('navigation-menu--open');
+    	}
+  	});
+	$("#mobile-navigation").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+	$("#navigation").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 
 
 });
